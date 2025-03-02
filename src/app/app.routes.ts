a54037baa3665bed57ff84e9e3dce4fb86c69704
Ronaldo Pires <ronaldo.vmagro@gmail.com>
2025-03-02 13:58:08 -0300

    Create page to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then((c) => c.LoginComponent),
+ },
+];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router, RouterModule } from '@angular/router';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule, MatButtonModule, ReactiveFormsModule, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  hide = true;
+  form!: FormGroup;
+  btnLoad: boolean = false;
+  selectedClinic: any;
+  user: any
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  constructor(
+    private fb: FormBuilder,
+    private AuthService: AuthService,
+    private _snackbar: MatSnackBar,
+    private router: Router
+  ) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      username: [null, Validators.required],
+      password: [null, Validators.required]
+    })
+  }
+
+  login() {
+    this.AuthService.login(this.form.value).subscribe({
+      next: (res: any) => {
+        this.user = res?.data
+        this.btnLoad = false;
+        this._snackbar.open('Deu certo! O usuário foi logado com sucesso.', 'X', {
+          horizontalPosition: this.horizontalPosition,
+          verticalPosition: this.verticalPosition,
+          panelClass: ['snackbar-success'],
+          duration: 4000,
+        })
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err: any) => {
+        this.btnLoad = false;
+        console.log(err);
+
+        this._snackbar.open(`${err.error.message}`, 'X', {
+          horizontalPosition: this.horizontalPosition,
+          verticalPosition: this.verticalPosition,
+          panelClass: ['snackbar-error'],
+          duration: 4000,
+        })
+      }
+    })
+  }
+}
